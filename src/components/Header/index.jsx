1bb3e11262f76fb46f46faca9b4efb91e5d38243
anthony-mini <anthony-mini@outlook.fr>
2022-12-05 17:07:49 +0100

    Create `header` `components`

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,22 @@
+import Logo from './../../assets/logo-kasa.svg';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header>
-      <h1>Kasa</h1>
+    <header className="header">
       <Link to="/">
-        <img src="./../../logo.svg" alt="alt" />
+        <img className="header__img" src={Logo} alt="Logo Kasa" />
       </Link>
+      <nav className="header__nav">
+        <ul className="header__nav--list">
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/about-us">A Propos</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
