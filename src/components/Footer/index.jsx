cceabb8208b63ea419ff2f83501ecab41f79c5c8
anthony-mini <anthony-mini@outlook.fr>
2022-12-05 19:55:45 +0100

    Add `footer` component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,10 @@
+import Logo from './../../assets/logo-kasa.svg';
+
 export default function Footer() {
     return (
-        <footer>
-            <p>Footer</p>
+        <footer className='footer'>
+            <img src={Logo} alt="Logo Kasa" />
+            <p>© 2020 Kasa. All rights reserved</p>
         </footer>
     )
 }
