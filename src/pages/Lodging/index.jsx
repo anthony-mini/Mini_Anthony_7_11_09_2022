5370199ca2b2f8329d61472763172f3fe202bf12
anthony-mini <anthony-mini@outlook.fr>
2022-12-09 17:35:33 +0100

    Add collapse for sheet lodging

--- src/pages/Lodging/index.jsx
+++ src/pages/Lodging/index.jsx
@@ -1,13 +1,41 @@
 import { useEffect } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import Lodgings from '../../data/lodgings.json';
+import Collapse from '../../components/Collapse';
 
-export default function Lodging() {
+export default function Lodging(props) {
+  // Get slot ID with `useParams` hook
+  const { id } = useParams();
+  const lodging = Lodgings.find((l) => l.id === id);
+
   useEffect(() => {
-    document.title = `Trouver votre logement - Kasa`;
+    if (lodging) {
+      // if lodging is well found, I modify the title
+      const title = lodging.title;
+      document.title = title + '  - Kasa';
+    }
   });
 
-  return (
-    <div>
-      <h1>Logements</h1>
-    </div>
+  console.log(id);
+
+  return lodging ? (
+    // If the lodging is found, we display this sheet
+    <main>
+      <div>
+        <Collapse
+          title="Description"
+          key={lodging.description}
+          description={lodging.description}
+        />
+        <Collapse className='name'
+          title="Équipements"
+          key={lodging.equipments}
+          equipments={lodging.equipments}
+        />
+      </div>
+    </main>
+  ) : (
+    // If the lodging is not found, the error page is displayed
+    <Navigate replace to="/*" />
   );
 }
