d60524b625381fbfba14f4710e3fa4f7c304dc35
anthony-mini <anthony-mini@outlook.fr>
2022-12-10 22:11:54 +0100

    comments delete

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-//import Header from '../Header';
-import Footer from '../Footer';
 import Home from '../../pages/Home';
 import About from '../../pages/About';
 import Lodging from '../../pages/Lodging';
 import Error from '../../pages/Error';
+import Footer from '../Footer';
 
 export default function Root() {
   return (
     <Router>
-      {/* <Header /> */}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about-us" element={<About />} />
