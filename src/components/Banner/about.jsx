ad16a578f5033216a39a6d510abf0cb88afbe2ac
anthony-mini <anthony-mini@outlook.fr>
2022-12-15 23:38:01 +0100

    removal of the two banner components

--- src/components/Banner/about.jsx
+++
@@ -1,7 +0,0 @@
-export default function Banner() {
-  return (
-    <section className="layout">
-      <div className="layout__conteneur"></div>
-    </section>
-  );
-}
--- src/components/Banner/home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Banner() {
-  return (
-    <section className="banner">
-      <div className="banner__conteneur">
-        <h1>Chez vous, partout et ailleurs</h1>
-      </div>
-    </section>
-  );
-}
