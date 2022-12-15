fa7c95b1fefb91919868b47b66d3bb8d9bc7ba35
anthony-mini <anthony-mini@outlook.fr>
2022-12-15 15:53:45 +0100

    code formatting with prettier

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -8,16 +8,26 @@ export default function Rating({ rating }) {
 
   return (
     // Retourne un élément div qui contient les étoiles
-    <div className='rating'>
+    <div className="rating">
       {grades.map((score, index) =>
         // Pour chaque note dans le tableau, crée une étoile en fonction de la note spécifiée.
         // La méthode map() explore toutes la notes présentes dans le tableau.
         rating >= score ? (
           // Tant que la note spécifiée est supérieure ou égale à la note actuelle, affiche une étoile pleine
-          <img key={index} className="rating--stars" src={SharpStar} alt="Sharp Star" />
+          <img
+            key={index}
+            className="rating--stars"
+            src={SharpStar}
+            alt="Sharp Star"
+          />
         ) : (
           // Sinon, affiche une étoile vide
-          <img key={index} src={ThinStar} className="rating--stars" alt="Thin Star" />
+          <img
+            key={index}
+            src={ThinStar}
+            className="rating--stars"
+            alt="Thin Star"
+          />
         )
       )}
     </div>
